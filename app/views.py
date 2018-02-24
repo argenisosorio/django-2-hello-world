4c63817f12e8ce4270fe9790e932a72b459b5746
Argenis Osorio <argenisosorio580@hotmail.com>
2018-02-23 20:49:56 -0400

    Added comments to the source code.

--- app/views.py
+++ app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+    """
+    Function that shows the hello world with an Http Response.
+    """
     return HttpResponse("Hello World with Django 2")
